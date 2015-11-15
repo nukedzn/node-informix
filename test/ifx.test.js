f8290271e399188f3e65b609fd1d9c1f972ff2e2
Uditha Atukorala <ua@nuked.zone>
2015-11-15 15:22:12 +0000

    basic test

---
+++ test/ifx.test.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+var expect = require( 'chai' ).expect;
+var ifx    = require( 'bindings' )( 'ifx' );
+
+
+describe( 'ifx', function () {
+
+	it( 'should have be able to connect to a database', function ( done ) {
+		ifx.connect( 'test', 'id-1', function ( err, conn ) {
+			expect( conn ).to.eql( 'id-1' );
+			done( err );
+		} )
+	} );
+
+} );
+
