8176c48e46e6ff27e1adf81a0872e7ed9f9029b0
Uditha Atukorala <ua@nuked.zone>
2015-11-26 16:45:24 +0000

    fix pass-by-reference for esqlc::prepare()

--- src/esqlc.h
+++ src/esqlc.h
@@ -12,7 +12,7 @@ class esqlc {
 public:
 
 	static int32_t connect( const char * connid, const char * database, const char * username = 0, const char * password = 0 );
-	static int32_t prepare( const char * stmtid, const char * stmtstr, ifx_sqlda_t * insqlda, ifx_sqlda_t * outsqlda );
+	static int32_t prepare( const char * stmtid, const char * stmtstr, ifx_sqlda_t * &insqlda, ifx_sqlda_t * &outsqlda );
 	static int32_t run( const char * stmtid, const char * curid, ifx_sqlda_t * insqlda );
 	static int32_t closeS( const char * stmtid );
 	static int32_t closeC( const char * curid );
