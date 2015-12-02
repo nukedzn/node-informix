2ec58c1eb29c76358a3dd4bd48cfbcbf162a0b22
Uditha Atukorala <ua@nuked.zone>
2015-12-02 19:26:44 +0000

    updated test db data

--- test/support/test-db.sql
+++ test/support/test-db.sql
@@ -3,9 +3,27 @@ connect to '@ol_informix1210' user 'informix' using 'informix';
 
 create database test;
 
+
 create table tcustomers (
 	id       serial,
 	fname    varchar(255),
 	lname    varchar(255)
 );
+
+create unique index icustomers_x1 on tcustomers(id);
+
+alter table tcustomers add constraint primary key (id)
+	constraint ccustomers_pk;
+
+
+create procedure pInsCustomer(
+	p_fname like tcustomers.fname,
+	p_lname like tcustomers.lname
+)
+returning int;
+
+	define v_id like tcustomers.id;
+	return v_id;
+
+end procedure;
 
